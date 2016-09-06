c5941d36b3d598b3317a7c4bd97c71ab03839fb5
Tomislav Bukal <tomislav.bukal@am2studio.hr>
2016-09-07 00:19:22 +0200

    Initial Commit

---
+++ Cookie-pop.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Cookie Pop
+Plugin URI: https://github.com/emailodroid/CookiePop
+Description: A brief description of the Plugin.
+Version: 0.1
+Author: Tomislav Bukal
+Author URI: https://twitter.com/TomiBukal
+License: A "Slug" license name e.g. GPL2
+*/
+	
